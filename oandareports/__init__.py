d6b8bee8a1d9f3072896ca15df847b55f2d59e28
Oeyvind Dale Sporck <spoerck@gmail.com>
2019-11-22 22:23:19 +0100

    Started on downloader for tradinghistory - Not working

---
+++ oandareports/__init__.py
@@ -0,0 +1,5 @@
+from luigi.parameter import Parameter
+from luigi import Task, ExternalTask
+from oandareports.helperfiles.task import TargetOutput, Requires, Requirement
+from oandareports.helperfiles.target import ParquetTarget
+
