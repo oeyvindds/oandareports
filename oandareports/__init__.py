18139c1b06620696f1c08bcaa60fdf59a19f6af9
Oeyvind Dale Sporck <spoerck@gmail.com>
2019-12-01 22:06:43 +0100

    Added __init__ files

--- oandareports/__init__.py
+++ oandareports/__init__.py
@@ -1,5 +1,6 @@
 from luigi.parameter import Parameter
 from luigi import Task, ExternalTask
+from oandareports import examples, helperfiles, reports, tests
 from oandareports.helperfiles.task import TargetOutput, Requires, Requirement
 from oandareports.helperfiles.target import ParquetTarget
 from dotenv import load_dotenv
