df0e5d20069e798bb9ca35607af752a88146c2a2
dliu936 <dli936@g.harvard.edu>
2019-12-12 16:42:51 -0500

    Updated url
    Fixed url for forked clone

--- oandareports/setup.py
+++ oandareports/setup.py
@@ -4,7 +4,7 @@ setup(
     name='oandareports',
     version='0.1.0',
     packages=['tests', 'reports', 'examples', 'helperfiles'],
-    url='https://github.com/dliu936/oandareports/tree/django',
+    url='https://github.com/dliu936/oandareports.git',
     license='MIT',
     author='Oyvind Sporck & Diana Liu',
     author_email='',
